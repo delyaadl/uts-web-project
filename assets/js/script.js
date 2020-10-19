306b4c4a0c94f8f34d8d9db295359350f0688098
delyaadl <delyatjung@gmail.com>
2020-10-20 00:48:59 +0700

    merging everyone's task and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
     
+    $('#myCarousel').carousel({
+		interval:   4000
+	});
+	
+	var clickEvent = false;
+	$('#myCarousel').on('click', '.nav a', function() {
+			clickEvent = true;
+			$('.nav li').removeClass('active');
+			$(this).parent().addClass('active');		
+	}).on('slid.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.nav').children().length -1;
+			var current = $('.nav li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.nav li').first().addClass('active');	
+			}
+		}
+		clickEvent = false;
+	});
     
     /* animation scrolling */
     $('.js--carousel').waypoint(function(direction) {
@@ -49,5 +70,6 @@ $(document).ready(function() {
     }, {
         offset: '70%'
     }) 
+
     
 });
